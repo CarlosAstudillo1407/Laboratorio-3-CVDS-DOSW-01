798f4e6fad75b564056efc2825898a12a9afb59b
carlos astudillo <carlos.astudillo-c@mail.escuelaing.edu.co>
2025-08-29 03:04:20 -0500

    se añadio proyecto maven

---
+++ src/main/java/edu/dosw/lab/Laboratorio_3_CVDS_DOSW_01/Laboratorio3CvdsDosw01Application.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.Laboratorio_3_CVDS_DOSW_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio3CvdsDosw01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio3CvdsDosw01Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/lab/Laboratorio_3_CVDS_DOSW_01/Laboratorio3CvdsDosw01ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.Laboratorio_3_CVDS_DOSW_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio3CvdsDosw01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
